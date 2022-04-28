7348f2b6382120cfce382c007f5b1ba6a340ead9
ebizzinfotechs <81732286+ebizzinfotechs@users.noreply.github.com>
2022-04-28 15:53:49 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,6 +40,7 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <h1>Storage logs error checking</h1>
+                    <a href="www.facebook.com" class="text-sm text-gray-700 underline">FACEBOOK</a>
                 </div>
 
             </div>
