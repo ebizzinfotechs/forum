f1671c58c6f29c02fcee83f7f6a78eceaa232ab3
ebizzinfotechs <81732286+ebizzinfotechs@users.noreply.github.com>
2022-04-28 15:34:52 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel-App</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -39,7 +39,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1>Welcome to the Laravel CI/CD</h1>
+                    <h1>Welcome to the Laravel Application</h1>
                 </div>
 
             </div>
