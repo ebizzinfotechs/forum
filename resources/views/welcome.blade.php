983fd6f81b44b6ed2919a85aaa95a8b1b80caa5b
ebizzinfotechs <81732286+ebizzinfotechs@users.noreply.github.com>
2022-04-29 10:10:53 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1>Same code on both serverscheck one</h1>
+                    <h1>Input section is working properly or not</h1>
                 </div>
 
             </div>
